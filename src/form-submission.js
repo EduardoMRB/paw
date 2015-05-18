96bf7084013276b99917c4196bd458d3dc31f396
Eduardo Borges <eduardomrb@gmail.com>
2015-05-18 08:41:02 -0400

    Polishing rough edges
    Deletes the main.js file (it was blank) and adds a verification for the
    form wrapper DOM node on form-submitssion.js.

--- src/form-submission.js
+++ src/form-submission.js
@@ -3,9 +3,12 @@
   let formContainer = document.querySelector(".form-wrapper");
   let loadStateContainer = document.querySelector("#load-state");
 
-  form.addEventListener("submit", e => {
-    e.preventDefault();
-    formContainer.classList.add("hidden");
-    loadStateContainer.classList.add("show");
-  });
+  // The form wrapper doesn't exist on index.html
+  if (form) {
+    form.addEventListener("submit", e => {
+      e.preventDefault();
+      formContainer.classList.add("hidden");
+      loadStateContainer.classList.add("show");
+    });
+  }
 })(document);
